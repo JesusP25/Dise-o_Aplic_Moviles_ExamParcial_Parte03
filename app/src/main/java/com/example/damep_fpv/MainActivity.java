fa28467c393bd1d970f34a0999cf2fd897d8668b
JESUS PAYE <2113010211@untels.edu.pe>
2024-10-16 18:21:57 -0500

    EP objetos, acciones

--- app/src/main/java/com/example/damep_fpv/MainActivity.java
+++ app/src/main/java/com/example/damep_fpv/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.damep_fpv;
 
 import android.os.Bundle;
 
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -14,11 +19,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Button btn1 = findViewById(R.id.ButtonCrear);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Redireccionando a crear cuenta...", Toast.LENGTH_SHORT).show());
+
+
+
+        setContentView(R.layout.activity_main);
+        Button btn2 = findViewById(R.id.ButtonIngresar);
+        btn2.setOnClickListener(view -> Toast.makeText(this, "Ingresando a su cuenta...", Toast.LENGTH_SHORT).show());
+
+
+
     }
 }
